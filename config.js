c83203698b61881d30ef03f6860a47420d08e226
yrahate7 <yrahate7@gmail.com>
2022-11-20 19:16:12 +0530

    Print log

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+console.log(process.env.PORT, process.env.WEBHOOK_SECRET, process.env.REPOSITORIES_FULL_PATH,);
+
 module.exports = {
     PORT: process.env.PORT ?? 3000,
     WEBHOOK_SECRET: process.env.WEBHOOK_SECRET,
